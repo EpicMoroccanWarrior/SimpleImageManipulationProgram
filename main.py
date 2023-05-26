a2376cca8c709eb57857dde7ba82bdc1c6a07ed6
EpicMoroccanWarrior <97562298+EpicMoroccanWarrior@users.noreply.github.com>
2023-05-26 20:10:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ IMP.show_image_in_mode("./Untitled.jpg", "Invert")
 IMP.show_image_in_mode("./Untitled.jpg", "FlipHorizontal")
 IMP.show_image_in_mode("./Untitled.jpg", "FlipVertical")
 IMP.show_image("./grey.jpg")
-IMP.show_image_in_mode("./grey.jpg", "GrayscaleToBw", 128) #the threshold is variable, the default value is 128
+IMP.show_image_in_mode("./grey.jpg", "GrayscaleToBw", threshold = 128) #the threshold is variable, if not provided the default value is 128
+IMP.show_image_in_mode("./grey.jpg", "Frame", frame_width = 20) #the frame_width is variable, if not provided the default value is 10
