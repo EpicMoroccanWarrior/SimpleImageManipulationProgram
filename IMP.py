4a661bc12a8f705230b9175e6efb9ef32f80b92a
EpicMoroccanWarrior <97562298+EpicMoroccanWarrior@users.noreply.github.com>
2023-05-26 20:11:00 +0100

    Update IMP.py

--- IMP.py
+++ IMP.py
@@ -21,12 +21,20 @@ def grayscale_to_bw(image_path, threshold):
     bw_image = np.where(image >= threshold, 255, 0) 
     return bw_image
 
+def add_black_frame(image_path, frame_width = 10):
+    image = plt.imread(image_path)
+    height, width, _ = image.shape
+    framed_image = np.zeros((height + 2 * frame_width, width + 2 * frame_width, 3), dtype=np.uint8)
+    framed_image[frame_width:-frame_width, frame_width:-frame_width, :] = image
+    return framed_image
+
+
 def display(img):
     plt.imshow(img)
     plt.axis("off")
     plt.show()
     
-def show_image_in_mode(path, mode, threshold = 128):
+def show_image_in_mode(path, mode, threshold = 128, frame_width = 10):
     match mode:
         case "Invert":
             display(Invert(path))
@@ -36,6 +44,8 @@ def show_image_in_mode(path, mode, threshold = 128):
             display(grayscale_to_bw(path, threshold))
         case "FlipVertical": 
             display(flip_image_vertically(path))
+        case "Frame":
+            display(add_black_frame(path, frame_width))
 
 def show_image(img):
     img = plt.imread(img)
